c192233001a65b50eabf3be03a7299685c33075e
Aymará <aymara@sapuru.com.ar>
2017-04-09 12:30:40 -0300

    Agrego modales
    Uso fondo nuevo y arreglo detalles de fondos

--- js/main.js
+++ js/main.js
@@ -139,6 +139,9 @@ jQuery(document).ready(function(){
 
 		};
 	};
+
+	//desde aquí copié y pegué otro código
+
 	var modalTrigger = $('.cd-modal-trigger'),
 		transitionLayer = $('.cd-transition-layer'),
 		transitionBackground = transitionLayer.children(),
@@ -198,4 +201,125 @@ jQuery(document).ready(function(){
 
 		resize = false;
 	}
+
+	var modalTrigger2 = $('.cd-modal-trigger-2'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow2 = $('.cd-modal-2');
+
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = 25, //number of png frames
+		resize = false;
+
+	//set transitionBackground dimentions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+	modalTrigger2.on('click', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('visible opening');
+		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
+		setTimeout(function(){
+			modalWindow2.addClass('visible');
+		}, delay);
+	});
+
+	//close modal window
+	modalWindow2.on('click', '.modal-close-2', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow2.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
+
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
+
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
+		} else {
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
+		}
+
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
+		});
+
+		resize = false;
+	}
+
+	var modalTrigger3 = $('.cd-modal-trigger-3'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow3 = $('.cd-modal-3');
+
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = 25, //number of png frames
+		resize = false;
+
+	//set transitionBackground dimentions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+	modalTrigger3.on('click', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('visible opening');
+		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
+		setTimeout(function(){
+			modalWindow3.addClass('visible');
+		}, delay);
+	});
+
+	//close modal window
+	modalWindow3.on('click', '.modal-close-3', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow3.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
+
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
+
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
+		} else {
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
+		}
+
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
+		});
+
+		resize = false;
+	}
+
 });
